6361548014d8d925590261f1cd6dc5e00e51cac8
Marcin Pajdzik <marcin.pajdzik@codewriters.co.uk>
2012-04-20 11:50:20 +0100

    added js code for the second popup

--- js/eu_cookie_compliance.js
+++ js/eu_cookie_compliance.js
@@ -1,12 +1,20 @@
-Drupal.behaviors.eu_cookie_compliance_popup = function(context) {  
-  if (!Drupal.settings.eu_cookie_compliance.popup_enabled) return;
-  if (eu_cookie_compliance_has_already_agreed()) return; 
+Drupal.behaviors.eu_cookie_compliance_popup = function(context) {   
+  enabled = Drupal.settings.eu_cookie_compliance.popup_enabled;
+  if(!enabled) return
+  
+  action = eu_cookie_compliance_has_already_agreed();
+  
+  if (!action) {
+    html = Drupal.settings.eu_cookie_compliance.popup_html_info;
+  } else if(action == 1) {
+    html = Drupal.settings.eu_cookie_compliance.popup_html_agreed;
+  } else {
+    return;
+  }
 
-  html = Drupal.settings.eu_cookie_compliance.popup_html;
   height = Drupal.settings.eu_cookie_compliance.popup_height;
   width = Drupal.settings.eu_cookie_compliance.popup_width;
   delay = Drupal.settings.eu_cookie_compliance.popup_delay;
-
   eu_cookie_compliance_create_popup(html, height, delay);
  
 }
@@ -24,18 +32,25 @@ function eu_cookie_compliance_create_popup(html, height, delay) {
   popup.show().animate( { bottom: 0 }, delay);
 }
 
-  function eu_cookie_compliance_has_already_agreed() { 
-    return document.cookie.indexOf("cookie-compliance-agreed") > -1; 
-  }
+function eu_cookie_compliance_has_already_agreed() { 
+  search = 'cookie-agreed=';
+  offset = document.cookie.indexOf(search);
+  if (offset < 1) return 0;
+  offset += search.length;
+  end = document.cookie.indexOf(';', offset);
+  if (end == -1) end = document.cookie.length;
+  value = document.cookie.substring(offset, end);
+  return value;
+}
 
-  function eu_cookie_compliance_has_agreed() { 
-    var date = new Date(); 
-    date.setDate(date.getDate() + 100); 
-    document.cookie = "cookie-compliance-agreed=true;expires=" + date.toUTCString() + ";path=/";
-    delay = Drupal.settings.eu_cookie_compliance.popup_delay;
-    eu_cookie_compliance_destroy_popup(delay)
-  }
-  
-  function eu_cookie_compliance_destroy_popup(delay) {
-    $("#sliding-popup").animate({ bottom: $("#sliding-popup").height() * -1 }, delay, function () { $("#sliding-popup").remove(); })
-  }
+function eu_cookie_compliance_has_agreed(value) { 
+  var date = new Date(); 
+  date.setDate(date.getDate() + 100); 
+  document.cookie = "cookie-agreed="+value+";expires=" + date.toUTCString() + ";path=/";
+  eu_cookie_compliance_destroy_popup(Drupal.settings.eu_cookie_compliance.popup_delay)
+  Drupal.behaviors.eu_cookie_compliance_popup();
+}
+
+function eu_cookie_compliance_destroy_popup(delay) {
+  $("#sliding-popup").animate({ bottom: $("#sliding-popup").height() * -1 }, delay, function () { $("#sliding-popup").remove(); })
+}
