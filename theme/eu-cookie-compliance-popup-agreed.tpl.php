1403e81723cca9d474cfd1a2ea42d9b3642ad04f
Marcin Pajdzik <marcin.pajdzik@codewriters.co.uk>
2012-04-20 11:51:00 +0100

    templates update

--- theme/eu-cookie-compliance-popup-agreed.tpl.php
+++ theme/eu-cookie-compliance-popup-agreed.tpl.php
@@ -1,9 +1,11 @@
 <div>  
-  <div class ="popup-content">
-    <p><?php print $message ?></p>
+  <div class ="popup-content agreed">
+    <div id="popup-text">
+      <?php print $message ?>
+    </div>
     <div id="popup-buttons">
-      <button type="button" onclick="javascript:eu_cookie_compliance_has_agreed()">Yes, I agree</button>
-      <button type="button" <?php if ($link) : ?> onclick="window.location.href='<?php print $link ?>'" <?php endif ?> >No, I want to find out more</button>
+      <button type="button" onclick="javascript:eu_cookie_compliance_has_agreed(2)">Hide this message</button>
+      <button type="button" <?php if ($link) : ?> onclick="window.location.href='<?php print $link ?>'" <?php endif ?> >More information on cookies</button>
     </div>
   </div>
 </div>
--- theme/eu-cookie-compliance-popup-info.tpl.php
+++ theme/eu-cookie-compliance-popup-info.tpl.php
@@ -1,10 +1,10 @@
 <div>  
-  <div class ="popup-content">
+  <div class ="popup-content info">
     <div id="popup-text">      
       <?php print $message ?>
     </div>
     <div id="popup-buttons">
-      <button type="button" onclick="javascript:eu_cookie_compliance_has_agreed()">Yes, I agree</button>
+      <button type="button" onclick="javascript:eu_cookie_compliance_has_agreed(1)">Yes, I agree</button>
       <button type="button" <?php if ($link) : ?> onclick="window.location.href='<?php print $link ?>'" <?php endif ?> >No, I want to find out more</button>
     </div>
   </div>
