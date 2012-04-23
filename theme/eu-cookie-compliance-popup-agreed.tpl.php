3b5fd805baaa2d302412073c8a160d38026b269f
Marcin Pajdzik <marcin.pajdzik@codewriters.co.uk>
2012-04-23 11:28:12 +0100

    changes suggested by the coder module.

--- theme/eu-cookie-compliance-popup-agreed.tpl.php
+++ theme/eu-cookie-compliance-popup-agreed.tpl.php
@@ -1,4 +1,21 @@
-<div>  
+<?php
+/**
+ * @file
+ * This is a template file for a pop-up informing a user that he has already agreed
+ * to cookies.
+ *
+ * When overriding this template it is important to note that jQuery will use the
+ * following classes to assign actions to buttons:
+ *
+ *
+ * agree-button      - agree to setting cookies
+ * hide-popup-button - destroy the pop-up
+ * find-more-button  - link to information page
+ *
+ */
+?>
+
+<div>
   <div class ="popup-content agreed">
     <div id="popup-text">
       <?php print $message ?>
--- theme/eu-cookie-compliance-popup-info.tpl.php
+++ theme/eu-cookie-compliance-popup-info.tpl.php
@@ -1,6 +1,23 @@
-<div>  
+<?php
+/**
+ * @file
+ * This is a template file for a pop-up prompting user to give their consent for
+ * the website to set cookies.
+ *
+ * When overriding this template it is important to note that jQuery will use the
+ * following classes to assign actions to buttons:
+ *
+ *
+ * agree-button      - agree to setting cookies
+ * hide-popup-button - destroy the pop-up
+ * find-more-button  - link to information page
+ *
+ */
+?>
+
+<div>
   <div class ="popup-content info">
-    <div id="popup-text">      
+    <div id="popup-text">
       <?php print $message ?>
     </div>
     <div id="popup-buttons">
