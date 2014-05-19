63a50b45440d13494a94d09fbab14cf232599512
Sven Berg Ryen <svenryen@gmail.com>
2014-05-19 13:27:01 +0200

    Reversed the patch in 38a24b0 (Issue #2082387) since it broke the auto-accept functionality.

--- js/eu_cookie_compliance.js
+++ js/eu_cookie_compliance.js
@@ -16,9 +16,33 @@
           var popup_hide_agreed = Drupal.settings.eu_cookie_compliance.popup_hide_agreed;
           if (status == 0) {
             var next_status = 1;
+            if (clicking_confirms) {
+              $('a, input[type=submit]').bind('click.eu_cookie_compliance', function(){
+                if(!agreed_enabled) {
+                  Drupal.eu_cookie_compliance.setStatus(1);
+                  next_status = 2;
+                }
+                Drupal.eu_cookie_compliance.changeStatus(next_status);
+              });
+            }
+
+            $('.agree-button').click(function(){
+              if(!agreed_enabled) {
+                Drupal.eu_cookie_compliance.setStatus(1);
+                next_status = 2;
+              }
+              Drupal.eu_cookie_compliance.changeStatus(next_status);
+            });
+
             Drupal.eu_cookie_compliance.createPopup(Drupal.settings.eu_cookie_compliance.popup_html_info);
           } else if(status == 1) {
             Drupal.eu_cookie_compliance.createPopup(Drupal.settings.eu_cookie_compliance.popup_html_agreed);
+            if (popup_hide_agreed) {
+              $('a, input[type=submit]').bind('click.eu_cookie_compliance_hideagreed', function(){
+                Drupal.eu_cookie_compliance.changeStatus(2);
+              });
+            }
+
           } else {
             return;
           }
