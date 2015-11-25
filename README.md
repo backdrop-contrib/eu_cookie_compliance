64e9edc99c5e40059595c79e839438949624fa07
Graham-72 <g.oliver@btinternet.com>
2015-11-25 05:53:32 +0000

    Initial porting to Backdrop

---
+++ README.md
@@ -0,0 +1,68 @@
+# EU Cookie Compliance
+
+The EU Cookie Compliance module enables a website to meet the
+requirements of the EU Directive on Privacy and Electronic Communications
+regarding the use of cookies.
+
+If a website makes use of cookies other than for the temporary storage of
+data while a transaction is in progress, the user must be informed and
+given the opportunity to decline the use of cookies. The site must also
+include an explanation of what cookies are used.
+
+This is a port from the Drupal module of the same name. 
+
+##How it works.
+
+The module displays a pup-up at the bottom or at the top of website to make 
+users aware of the fact that cookies are being set. The user may then give 
+his/her consent or move to a page that provides more details. Consent is given 
+by the user pressing the agree buttons or by continuing browsing the website. 
+Once consent is given another pop-up appears with a thank you message.
+
+The module provides a settings page where the pop-up can be customised. There 
+are also template files for the pop-ups that can be overridden by your theme. 
+
+## Status
+
+This is an initial port of the module ready for testing.
+
+## Installation
+
+Install this module using the official Backdrop CMS instructions at
+  https://backdropcms.org/guide/modules.
+  
+    
+## Configuration
+
+Navigate to module settings at admin/config/system/eu_cookie_compliance.
+
+
+## Help & Documentation
+
+See readme.txt for more information about installation and use.
+
+
+## License
+
+This project is GPL v2 software. See the LICENSE.txt file in this
+directory for complete text.
+    
+        
+## Current porting to Backdrop
+
+Graham Oliver (github.com/Graham-72/)
+
+## Credits
+
+### Maintainers for Drupal:
+
+- Sven Berg Ryen (svenryen)
+- id.tornado
+- Marcin Pajdzik (marcin-pajdzik)
+- Achton Smidt Winther (achton)
+- mibfire
+
+### Acknowledgement
+
+This port to Backdrop would not, of course, be possible without all
+the work done by the developers and maintainers of the Drupal module.
--- js/eu_cookie_compliance.js
+++ js/eu_cookie_compliance.js
@@ -1,45 +1,45 @@
 (function ($) {
-  Drupal.behaviors.eu_cookie_compliance_popup = {
+  Backdrop.behaviors.eu_cookie_compliance_popup = {
     attach: function(context, settings) {
       $('body').not('.sliding-popup-processed').addClass('sliding-popup-processed').each(function() {
         try {
-          var enabled = Drupal.settings.eu_cookie_compliance.popup_enabled;
+          var enabled = Backdrop.settings.eu_cookie_compliance.popup_enabled;
           if(!enabled) {
             return;
           }
-          if (!Drupal.eu_cookie_compliance.cookiesEnabled()) {
+          if (!Backdrop.eu_cookie_compliance.cookiesEnabled()) {
             return;
           } 
-          var status = Drupal.eu_cookie_compliance.getCurrentStatus();
-          var clicking_confirms = Drupal.settings.eu_cookie_compliance.popup_clicking_confirmation;
-          var agreed_enabled = Drupal.settings.eu_cookie_compliance.popup_agreed_enabled;
-          var popup_hide_agreed = Drupal.settings.eu_cookie_compliance.popup_hide_agreed;
+          var status = Backdrop.eu_cookie_compliance.getCurrentStatus();
+          var clicking_confirms = Backdrop.settings.eu_cookie_compliance.popup_clicking_confirmation;
+          var agreed_enabled = Backdrop.settings.eu_cookie_compliance.popup_agreed_enabled;
+          var popup_hide_agreed = Backdrop.settings.eu_cookie_compliance.popup_hide_agreed;
           if (status == 0) {
             var next_status = 1;
             if (clicking_confirms) {
               $('a, input[type=submit]').bind('click.eu_cookie_compliance', function(){
                 if(!agreed_enabled) {
-                  Drupal.eu_cookie_compliance.setStatus(1);
+                  Backdrop.eu_cookie_compliance.setStatus(1);
                   next_status = 2;
                 }
-                Drupal.eu_cookie_compliance.changeStatus(next_status);
+                Backdrop.eu_cookie_compliance.changeStatus(next_status);
               });
             }
 
             $('.agree-button').click(function(){
               if(!agreed_enabled) {
-                Drupal.eu_cookie_compliance.setStatus(1);
+                Backdrop.eu_cookie_compliance.setStatus(1);
                 next_status = 2;
               }
-              Drupal.eu_cookie_compliance.changeStatus(next_status);
+              Backdrop.eu_cookie_compliance.changeStatus(next_status);
             });
 
-            Drupal.eu_cookie_compliance.createPopup(Drupal.settings.eu_cookie_compliance.popup_html_info);
+            Backdrop.eu_cookie_compliance.createPopup(Backdrop.settings.eu_cookie_compliance.popup_html_info);
           } else if(status == 1) {
-            Drupal.eu_cookie_compliance.createPopup(Drupal.settings.eu_cookie_compliance.popup_html_agreed);
+            Backdrop.eu_cookie_compliance.createPopup(Backdrop.settings.eu_cookie_compliance.popup_html_agreed);
             if (popup_hide_agreed) {
               $('a, input[type=submit]').bind('click.eu_cookie_compliance_hideagreed', function(){
-                Drupal.eu_cookie_compliance.changeStatus(2);
+                Backdrop.eu_cookie_compliance.changeStatus(2);
               });
             }
 
@@ -54,104 +54,104 @@
     }
   }
 
-  Drupal.eu_cookie_compliance = {};
+  Backdrop.eu_cookie_compliance = {};
 
-  Drupal.eu_cookie_compliance.createPopup = function(html) {
+  Backdrop.eu_cookie_compliance.createPopup = function(html) {
     var popup = $(html)
       .attr({"id": "sliding-popup"})
-      .height(Drupal.settings.eu_cookie_compliance.popup_height)
-      .width(Drupal.settings.eu_cookie_compliance.popup_width)
+      .height(Backdrop.settings.eu_cookie_compliance.popup_height)
+      .width(Backdrop.settings.eu_cookie_compliance.popup_width)
       .hide();
-    if(Drupal.settings.eu_cookie_compliance.popup_position) {
+    if(Backdrop.settings.eu_cookie_compliance.popup_position) {
       popup.prependTo("body");
       var height = popup.height();
       popup.show()
         .attr({"class": "sliding-popup-top"})
         .css({"top": -1 * height})
-        .animate({top: 0}, Drupal.settings.eu_cookie_compliance.popup_delay);
+        .animate({top: 0}, Backdrop.settings.eu_cookie_compliance.popup_delay);
     } else {
       popup.appendTo("body");
       height = popup.height();
       popup.show()
         .attr({"class": "sliding-popup-bottom"})
         .css({"bottom": -1 * height})
-        .animate({bottom: 0}, Drupal.settings.eu_cookie_compliance.popup_delay)
+        .animate({bottom: 0}, Backdrop.settings.eu_cookie_compliance.popup_delay)
     }
-    Drupal.eu_cookie_compliance.attachEvents();
+    Backdrop.eu_cookie_compliance.attachEvents();
   }
 
-  Drupal.eu_cookie_compliance.attachEvents = function() {
-	var clicking_confirms = Drupal.settings.eu_cookie_compliance.popup_clicking_confirmation;
-    var agreed_enabled = Drupal.settings.eu_cookie_compliance.popup_agreed_enabled;
+  Backdrop.eu_cookie_compliance.attachEvents = function() {
+	var clicking_confirms = Backdrop.settings.eu_cookie_compliance.popup_clicking_confirmation;
+    var agreed_enabled = Backdrop.settings.eu_cookie_compliance.popup_agreed_enabled;
     $('.find-more-button').click(function(){
-      if (Drupal.settings.eu_cookie_compliance.popup_link_new_window) {
-        window.open(Drupal.settings.eu_cookie_compliance.popup_link);
+      if (Backdrop.settings.eu_cookie_compliance.popup_link_new_window) {
+        window.open(Backdrop.settings.eu_cookie_compliance.popup_link);
       }
       else{
-        window.location.href = Drupal.settings.eu_cookie_compliance.popup_link;
+        window.location.href = Backdrop.settings.eu_cookie_compliance.popup_link;
       }
     });
     $('.agree-button').click(function(){
       var next_status = 1;
       if(!agreed_enabled) {
-        Drupal.eu_cookie_compliance.setStatus(1);
+        Backdrop.eu_cookie_compliance.setStatus(1);
         next_status = 2;
       }
       if (clicking_confirms) {
         $('a, input[type=submit]').unbind('click.eu_cookie_compliance');
       }
-      Drupal.eu_cookie_compliance.changeStatus(next_status);
+      Backdrop.eu_cookie_compliance.changeStatus(next_status);
     });
     $('.hide-popup-button').click(function(){
-      Drupal.eu_cookie_compliance.changeStatus(2);
+      Backdrop.eu_cookie_compliance.changeStatus(2);
     });
   }
 
-  Drupal.eu_cookie_compliance.getCurrentStatus = function() {
+  Backdrop.eu_cookie_compliance.getCurrentStatus = function() {
 	name = 'cookie-agreed';
-	value = Drupal.eu_cookie_compliance.getCookie(name);
+	value = Backdrop.eu_cookie_compliance.getCookie(name);
 	return value;
   }
 
-  Drupal.eu_cookie_compliance.changeStatus = function(value) {
-    var status = Drupal.eu_cookie_compliance.getCurrentStatus();
+  Backdrop.eu_cookie_compliance.changeStatus = function(value) {
+    var status = Backdrop.eu_cookie_compliance.getCurrentStatus();
     if (status == value) return;
-    if(Drupal.settings.eu_cookie_compliance.popup_position) {
-      $(".sliding-popup-top").animate({top: $("#sliding-popup").height() * -1}, Drupal.settings.eu_cookie_compliance.popup_delay, function () {
+    if(Backdrop.settings.eu_cookie_compliance.popup_position) {
+      $(".sliding-popup-top").animate({top: $("#sliding-popup").height() * -1}, Backdrop.settings.eu_cookie_compliance.popup_delay, function () {
         if(status == 0) {
-          $("#sliding-popup").html(Drupal.settings.eu_cookie_compliance.popup_html_agreed).animate({top: 0}, Drupal.settings.eu_cookie_compliance.popup_delay);
-          Drupal.eu_cookie_compliance.attachEvents();
+          $("#sliding-popup").html(Backdrop.settings.eu_cookie_compliance.popup_html_agreed).animate({top: 0}, Backdrop.settings.eu_cookie_compliance.popup_delay);
+          Backdrop.eu_cookie_compliance.attachEvents();
         }
         if(status == 1) {
           $("#sliding-popup").remove();
         }
       })
     } else {
-      $(".sliding-popup-bottom").animate({bottom: $("#sliding-popup").height() * -1}, Drupal.settings.eu_cookie_compliance.popup_delay, function () {
+      $(".sliding-popup-bottom").animate({bottom: $("#sliding-popup").height() * -1}, Backdrop.settings.eu_cookie_compliance.popup_delay, function () {
         if(status == 0) {
-          $("#sliding-popup").html(Drupal.settings.eu_cookie_compliance.popup_html_agreed).animate({bottom: 0}, Drupal.settings.eu_cookie_compliance.popup_delay)
-          Drupal.eu_cookie_compliance.attachEvents();
+          $("#sliding-popup").html(Backdrop.settings.eu_cookie_compliance.popup_html_agreed).animate({bottom: 0}, Backdrop.settings.eu_cookie_compliance.popup_delay)
+          Backdrop.eu_cookie_compliance.attachEvents();
         }
         if(status == 1) {
           $("#sliding-popup").remove();
         }
       ;})
     }
-    Drupal.eu_cookie_compliance.setStatus(value);
+    Backdrop.eu_cookie_compliance.setStatus(value);
   }
 
-  Drupal.eu_cookie_compliance.setStatus = function(status) {
+  Backdrop.eu_cookie_compliance.setStatus = function(status) {
     var date = new Date();
-    date.setDate(date.getDate() + parseInt(Drupal.settings.eu_cookie_compliance.cookie_lifetime));
-    var cookie = "cookie-agreed=" + status + ";expires=" + date.toUTCString() + ";path=" + Drupal.settings.basePath;
-    if(Drupal.settings.eu_cookie_compliance.domain) {
-      cookie += ";domain="+Drupal.settings.eu_cookie_compliance.domain;
+    date.setDate(date.getDate() + parseInt(Backdrop.settings.eu_cookie_compliance.cookie_lifetime));
+    var cookie = "cookie-agreed=" + status + ";expires=" + date.toUTCString() + ";path=" + Backdrop.settings.basePath;
+    if(Backdrop.settings.eu_cookie_compliance.domain) {
+      cookie += ";domain="+Backdrop.settings.eu_cookie_compliance.domain;
     }
     document.cookie = cookie;
   }
 
-  Drupal.eu_cookie_compliance.hasAgreed = function() {
-    var status = Drupal.eu_cookie_compliance.getCurrentStatus();
+  Backdrop.eu_cookie_compliance.hasAgreed = function() {
+    var status = Backdrop.eu_cookie_compliance.getCurrentStatus();
     if(status == 1 || status == 2) {
       return true;
     }
@@ -160,9 +160,9 @@
 
 
   /**
-   * Verbatim copy of Drupal.comment.getCookie().
+   * Verbatim copy of Backdrop.comment.getCookie().
    */
-  Drupal.eu_cookie_compliance.getCookie = function(name) {
+  Backdrop.eu_cookie_compliance.getCookie = function(name) {
     var search = name + '=';
     var returnValue = '';
 
@@ -181,7 +181,7 @@
     return returnValue;
   };
   
-  Drupal.eu_cookie_compliance.cookiesEnabled = function() {
+  Backdrop.eu_cookie_compliance.cookiesEnabled = function() {
     var cookieEnabled = (navigator.cookieEnabled) ? true : false;
       if (typeof navigator.cookieEnabled == "undefined" && !cookieEnabled) { 
         document.cookie="testcookie";
