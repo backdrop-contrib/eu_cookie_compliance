57a992ac132b5da2aee19312f5463aecee577fc4
Jen Lampton <jen@jeneration.com>
2019-03-06 16:28:04 -0800

    Remove one instance of Drupal by cleaning up documentation.

--- eu_cookie_compliance.api.php
+++ eu_cookie_compliance.api.php
@@ -4,8 +4,7 @@
  * Documentation for the EU Cookie Compliance module.
  *
  * This file contains no working PHP code; it exists to provide additional
- * documentation for doxygen as well as to document hooks in the standard
- * Drupal manner.
+ * documentation for doxygen as well as to document hooks.
  */
 
 /**
